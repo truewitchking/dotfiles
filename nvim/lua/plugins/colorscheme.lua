2a1ac644ff5361b4bac413167db339f1ddf78cef
witchking <truewitchking@protonmail.com>
2025-10-08 00:56:36 +0300

    Update

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,23 @@
 return {
-    'catppuccin/nvim',
-        name = 'catppuccin',
-    priority = 1000,
-    lazy = false,
-    opts = {
-        flavour = 'mocha',
-        transparent_background = true,
-    },
-    config = function(opts)
-        require('catppuccin').setup(opts)
-        vim.cmd.colorscheme('catppuccin')
-    end,
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		flavour = "mocha",
+		transparent_background = true,
+		integrations = {
+			alpha = true,
+			blink_cmp = {
+				style = "bordered",
+			},
+		},
+	},
+	config = function(opts)
+		require("catppuccin").setup(opts)
+		vim.cmd.colorscheme("catppuccin")
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+	end,
 }
