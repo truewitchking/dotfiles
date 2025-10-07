7caad9c99c50d3e9944df3fd346d394e1d833186
witchking <truewitchking@protonmail.com>
2025-10-08 00:05:20 +0300

    Update

---
+++ nvim/lua/plugins/leetcode.lua
@@ -0,0 +1,10 @@
+return {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+    },
+    opts = {
+    },
+}
